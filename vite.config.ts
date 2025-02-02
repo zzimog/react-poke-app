abc5a4e9b32b955aca216108433aed5631088561
zzimog <null>
2025-02-02 21:06:30 +0100

    deploy yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-poke-app/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
